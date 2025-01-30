bf764b0f4d3c2cbc60d22f785863c58b0b0a3172
memes-forever <vladi.1153@gmail.com>
2025-01-30 11:51:05 +0300

    update project

--- project/flow/migration/2.roles&users.sql
+++ project/flow/migration/2.roles&users.sql
@@ -1,8 +1,11 @@
+/* roles */
 CREATE ROLE IF NOT EXISTS airflow_group ON CLUSTER sharded_cluster;
-
 GRANT SELECT ON *.* TO airflow_group ON CLUSTER sharded_cluster;
 GRANT CLUSTER ON *.* TO airflow_group ON CLUSTER sharded_cluster;
+
+/* databases access */
 GRANT ALL ON snp_gharchive.* TO airflow_group ON CLUSTER sharded_cluster;
 
+/* users */
 CREATE USER IF NOT EXISTS airflow_user IDENTIFIED WITH plaintext_password BY 'airflow_password' ON CLUSTER sharded_cluster;
 GRANT airflow_group TO airflow_user ON CLUSTER sharded_cluster;
