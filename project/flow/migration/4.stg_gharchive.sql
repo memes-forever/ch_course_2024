746dad6d61ed8e16d87f3ff0c9d4f95ff62d1c2e
memes-forever <vladi.1153@gmail.com>
2025-02-07 16:10:05 +0300

    update project

--- project/flow/migration/4.stg_gharchive.sql
+++ project/flow/migration/4.stg_gharchive.sql
@@ -16,3 +16,27 @@ CREATE TABLE IF NOT EXISTS stg_gharchive.events ON CLUSTER sharded_cluster
 AS stg_gharchive._events
 ENGINE = Distributed('sharded_cluster', 'stg_gharchive', '_events', xxHash64(`date_load`))  -- distributed key
 ;
+
+
+CREATE TABLE IF NOT EXISTS stg_gharchive._actor_and_org ON CLUSTER sharded_cluster
+(
+    `date_load` DateTime,
+    `type` Nullable(String),
+    `event_id` Nullable(UInt64),
+    `id` Nullable(UInt64),
+    `login` Nullable(String),
+    `display_login` Nullable(String),
+    `gravatar_id` Nullable(String),
+    `url` Nullable(String),
+    `avatar_url` Nullable(String),
+    `proccessed_dttm` DateTime DEFAULT now()  -- tech field
+)
+ENGINE = ReplicatedMergeTree('/clickhouse/tables/{database}/{table}/{uuid}/{shard}', '{replica}')
+ORDER BY date_load
+SETTINGS index_granularity = 8192
+;
+
+CREATE TABLE IF NOT EXISTS stg_gharchive.actor_and_org ON CLUSTER sharded_cluster
+AS stg_gharchive._actor_and_org
+ENGINE = Distributed('sharded_cluster', 'stg_gharchive', '_actor_and_org', xxHash64(`date_load`))  -- distributed key
+;
